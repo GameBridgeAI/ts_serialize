a53dbbc2bf920066cb0f45469818e62f441e8ce0
Scott Hardy <scott.hardy@mindbridge.ai>
2020-06-11 02:24:04 -0400

    add test for shell script

--- .github/workflows/scripts/create_npm_package_file_test.sh
+++ .github/workflows/scripts/create_npm_package_file_test.sh
@@ -3,10 +3,13 @@
 # Copyright 2018-2020 Gamebridge.ai authors. All rights reserved. MIT license.
 
 # @description test build functions for package.json file from a github tag varialbes
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-# echo $DIR;exit
+
 mkdir "$DIR/test_tmp"
+
 pushd "$DIR/test_tmp" > /dev/null
+
 if [ ! "$DIR/create_npm_package_file.sh" ]; then
   echo "Test Error: create_npm_package_file ran without a tag version"
   exit 1
@@ -26,4 +29,5 @@ if [ "$(jq -r ".version" "$DIR/test_tmp/package.json")" != "$test_version" ]; th
 fi
 
 popd > /dev/null
+
 rm -rf "$DIR/test_tmp"
