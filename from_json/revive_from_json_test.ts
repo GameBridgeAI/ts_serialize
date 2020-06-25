b4b097c9b09e60339307e6be342d149206b81f44
Scott Hardy <scott.hardy@mindbridge.ai>
2020-06-25 03:13:24 -0400

    remove unneeded test

--- from_json/revive_from_json_test.ts
+++ from_json/revive_from_json_test.ts
@@ -32,38 +32,16 @@ test({
       )
       test2 = new Test1();
     }
+
     class Test3 extends Test2 {
       @SerializeProperty("test_three")
       test3 = false;
     }
+
     const test = new Test3().fromJson(
       `{"test_three":true,"test_two":{"test_one":false}}`,
     );
-    assertEquals(test.test2.test1, false);
-  },
-});
 
-test({
-  name: "fromJsonAs works in nested properties",
-  fn() {
-    class Test1 extends Serializable {
-      @SerializeProperty("test_one")
-      test1 = true;
-    }
-
-    class Test2 extends Serializable {
-      @SerializeProperty(
-        { serializedKey: "test_two", fromJsonStrategy: fromJsonAs(Test1) },
-      )
-      test2 = new Test1();
-    }
-    class Test3 extends Test2 {
-      @SerializeProperty("test_three")
-      test3 = false;
-    }
-    const test = new Test3().fromJson(
-      `{"test_three":true,"test_two":{"test_one":false}}`,
-    );
     assertEquals(test.test3, true);
     assert(test.test2 instanceof Test1);
     assertEquals(test.test2.test1, false);
