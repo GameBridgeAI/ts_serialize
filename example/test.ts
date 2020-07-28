272b22593219a006a5b965af0fc2ce936c1d6467
Scott Hardy <scott.hardy@mindbridge.ai>
2020-07-28 00:42:18 -0400

    deleette unneeded code

--- example/test.ts
+++
@@ -1,9 +0,0 @@
-import { Serializable, SerializeProperty } from "ts_serialize";
-
-class Test extends Serializable {
-  @SerializeProperty()
-  testName = "toJson";
-}
-
-console.log(new Test().toJson());
-console.log(new Test().fromJson(`{"testName":"fromJson"}`));
