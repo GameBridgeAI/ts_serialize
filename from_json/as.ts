9e4054f8cd6b0ecc51c748dc5019bc766e4ca365
Scott Hardy <scott.hardy@mindbridge.ai>
2020-09-14 19:16:18 -0400

    manual types updates

--- from_json/as.ts
+++ from_json/as.ts
@@ -3,7 +3,7 @@
 import { FromJsonStrategy, Serializable } from "../serializable.ts";
 
 /** revive data using `fromJson` on a subclass type */
-export function fromJsonAs<T extends Serializable>(
+export function fromJsonAs<T>(
   type: T & { new (): Serializable },
 ): FromJsonStrategy {
   return (value: T) => new type().fromJson(value);
