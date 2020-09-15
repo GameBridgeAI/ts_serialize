a40cd1cac299141042b7c2d916768374d675afcb
Scott Hardy <scott.hardy@mindbridge.ai>
2020-09-14 21:59:51 -0400

    moving more files

---
+++ .github/workflows/scripts/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Copyright 2018-2020 Gamebridge.ai authors. All rights reserved. MIT license.
+
+.github/workflows/scripts/npm_release_files/create_npm_package_file_test.sh
+
+curl -L https://deno.land/x/install/install.sh | sh -s "v1.4.0"
+
+export PATH="$HOME/.deno/bin:$PATH"
+
+mkdir dist
+
+deno run --unstable --allow-read ./.github/workflows/scripts/npm_release_files/babel_ts_serialize.ts > dist/ts_serialize.js
+
+cp .github/workflows/scripts/npm_release_files/ts_serialize.d.ts dist
+
+cp CHANGELOG.md dist
+
+cp README.md dist
+
+cp LICENSE dist
+
+cd dist
+
+../.github/workflows/scripts/npm_release_files/create_npm_package_file.sh ${{ steps.get_github_tag.outputs.VERSION }}
+
+npm install
+
+# npm link
+
+# cd examples/node
+
+# npm link @gamebridgeai/ts_serialize
+
+# npm i typescript
+
+# result="$(npm test)"
---
+++ examples/node/README.md
@@ -0,0 +1,13 @@
+# ts_serialize NodeJS Example
+
+From this directory run
+
+```
+$ npm i @gamebridgeai/ts_serialize typescript
+```
+
+Then run 
+
+```
+$ npm test
+```
