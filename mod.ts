da0edb93c17c9d9fa59d1baff94e997b02e43b11
Scott Hardy <scott.hardy@mindbridge.ai>
2020-11-06 01:45:29 -0500

    testing biulds again

--- mod.ts
+++ mod.ts
@@ -6,10 +6,14 @@
 export { SerializeProperty } from "./serialize_property.ts";
 
 /** types, isolatedModules requires this to be `export type ...` */
-export type { FromJsonStrategy, ToJsonStrategy } from "./serializable.ts";
+export type {
+  FromJsonStrategy,
+  ToJsonStrategy,
+  TransformKey,
+} from "./serializable.ts";
 
 /** abstract class and and compose strategy functions */
-export { composeStrategy, Serializable, TransformKey } from "./serializable.ts";
+export { composeStrategy, Serializable } from "./serializable.ts";
 
 /** from json strategies */
 export { fromJsonAs } from "./from_json/as.ts";
