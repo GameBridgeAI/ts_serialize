8fe071e5a125b69b34591c7e02f0d1dcea708fa6
Michael Smith <msmith@mindbridge.ai>
2020-11-07 22:57:10 -0500

    Fix semantic merge conflicts for JsonObject

--- from_json/as.ts
+++ from_json/as.ts
@@ -11,10 +11,10 @@ import {
 export function fromJsonAs<T>(
   type: T & { new (): Serializable },
 ): FromJsonStrategy {
-  return (value: T) => {
+  return (value: JsonValue) => {
     if (Array.isArray(value)) {
-      return value.map((item) => new type().fromJson(item));
+      return value.map((item) => new type().fromJson(item as JsonObject));
     }
-    return new type().fromJson(value);
+    return new type().fromJson(value as JsonObject);
   };
 }
