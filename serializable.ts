6b389258af829d0b275cc7b806262c4c9878a1cc
Michael Smith <msmith@mindbridge.ai>
2020-11-07 22:59:07 -0500

    Remove the ability to fromJson() nothing, as per a PR comment

--- serializable.ts
+++ serializable.ts
@@ -34,10 +34,9 @@ export abstract class Serializable {
   public toJson(): string {
     return toJson(this);
   }
-  public fromJson(): this;
   public fromJson(json: string): this;
   public fromJson(json: JsonObject): this;
-  public fromJson(json: string | JsonObject = {}): this {
+  public fromJson(json: string | JsonObject): this {
     return fromJson(this, json);
   }
 }
