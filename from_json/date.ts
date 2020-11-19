e2d64330a327458ad5a47c4beddbe93dddaab17a
Scott Hardy <scott.hardy@mindbridge.ai>
2020-11-19 04:05:26 -0500

    time and dates updates and tests

--- from_json/date.ts
+++ from_json/date.ts
@@ -4,85 +4,78 @@ import { FromJsonStrategy, JsonValue } from "../serializable.ts";
 
 /** allows authors to pass a regex to parse as a date */
 export function createDateStrategy(regex: RegExp): FromJsonStrategy {
-  return function _createDateStrategy(value: any): any | Date {
-    return typeof value === "string" && regex.exec(value)
-      ? new Date(value)
-      : value;
+  return function _createDateStrategy(input: JsonValue): Date {
+    if (typeof input !== "string" || !regex.exec(input)) {
+      throw new Error("invalid date");
+    }
+    return new Date(input);
   };
 }
 
-/** converts timezone to minutes */
-function parseTimezone(zone: number): number {
-  return zone * 60 - Number.parseInt(`-${new Date().getTimezoneOffset()}`);
-}
-
-interface IsoWeekDate {
-  year: number;
-  week: number;
-  weekday: number;
-  timezone: number;
-}
-
-/** Test a string for a full ISO 8601 Date */
+/** Test a string for a ISO 8601 Date */
 export function iso8601Date(input: JsonValue): any {
-  var iso =
-    /^(\d{4})(?:-?W(\d+)(?:-?(\d+)D?)?|(?:-(\d+))?-(\d+))(?:[T ](\d+):(\d+)(?::(\d+)(?:\.(\d+))?)?)?(?:Z(-?\d*))?$/;
-  // throw error on bad data?
+  /** a provided regex to deal with iso formats
+   * @example 2008-08-30T01:45:36
+   * @example 2008-08-30T01:45:36.123Z
+   * @see https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s07.html
+   * 
+   * We are accepting timezones but will depend on `Date.UTC`
+   */
+  const iso =
+    //        <year>              -    <month>    -         <day>   <T non optional> <hour>:    <minute>:    <second>    <.milli> <Z  or `-+timezone`>
+    /^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?$/;
+
   if (typeof input !== "string" || !iso.exec(input)) {
-    return input;
+    throw new Error("Invalid date");
   }
-  // first match is the whole pattern which we do not need `,` skips it
-  // we're also skipping week and weekday
+
+  /** the result array. first in the list is the full pattern
+   * @example 2020-12-31T12:00:00.300Z
+   * @capture ["2020-12-31T12:00:00.300Z","2020","12","31","12","00","00",".300","Z]
+   * @mapped [ 2020, 12, 31, 12, 0, 0, 0.3 ]
+   */
   let [
-      ,
-      year,
-      ,
-      ,
-      month,
-      day,
-      hour,
-      minute,
-      second,
-      millisecond,
-      zone,
-    ] = input.match(iso)?.map(Number) || [],
-    timezone = parseTimezone(0);
+    ,
+    // skipping the full pattern match above
+    year,
+    month,
+    day,
+    hour,
+    minute,
+    second,
+    millisecond,
+  ] = iso.exec(input)?.map(Number) || [];
 
   if (Number.isNaN(year)) {
-    return input;
+    throw new Error("Invalid date");
   }
 
   if (Number.isNaN(month)) {
     month = 1;
   }
-
+  if (Number.isNaN(day)) {
+    day = 0;
+  }
   if (Number.isNaN(hour)) {
     hour = 0;
   }
-
   if (Number.isNaN(minute)) {
     minute = 0;
   }
-
   if (Number.isNaN(second)) {
     second = 0;
   }
-
   if (Number.isNaN(millisecond)) {
     millisecond = 0;
   }
 
-  if (!Number.isNaN(zone)) {
-    timezone = parseTimezone(zone);
-  }
-
-  return new Date(
+  return new Date(Date.UTC(
     year,
     month - 1,
     day,
     hour,
-    minute - timezone,
+    minute,
     second,
-    millisecond,
-  );
+    millisecond * 1000,
+  ));
 }
--- from_json/date_test.ts
+++ from_json/date_test.ts
@@ -1,17 +1,37 @@
 // Copyright 2018-2020 Gamebridge.ai authors. All rights reserved. MIT license.
 
-import { assert, assertEquals, test } from "../test_deps.ts";
+import { assert, assertEquals, fail, test } from "../test_deps.ts";
 import { createDateStrategy, iso8601Date } from "./date.ts";
 import { Serializable, SerializeProperty } from "../mod.ts";
 
 test({
-  name: "createDateStrategy creates strategy from regex",
+  name: "createDateStrategy - creates strategy from regex",
   fn() {
-    const dateStrategy = createDateStrategy(/^(\d{4})-(\d{2})-(\d{2})$/);
-    const testJson = `{"date":"2099-11-25"}`;
-    const testObj = JSON.parse(testJson, (_, v) => dateStrategy(v));
+    class Test extends Serializable {
+      @SerializeProperty({
+        fromJsonStrategy: createDateStrategy(/^(\d{4})-(\d{2})-(\d{2})$/),
+      })
+      date!: Date;
+    }
+    const testObj = new Test().fromJson({ "date": "1990-11-11" });
     assert(testObj.date instanceof Date);
-    assertEquals(testObj.date.getFullYear(), 2099);
+    assertEquals(testObj.date.toISOString(), "1990-11-11T00:00:00.000Z");
+  },
+});
+
+test({
+  name: "createDateStrategy - Timezones applied by string types",
+  fn() {
+    class Test extends Serializable {
+      @SerializeProperty({
+        fromJsonStrategy: createDateStrategy(/^(\d{4})\/(\d{2})\/(\d{2})$/),
+      })
+      date!: Date;
+    }
+    const testObj = new Test().fromJson({ "date": "1990/11/11" });
+    assert(testObj.date instanceof Date);
+    assertEquals(testObj.date.toISOString(), "1990-11-11T05:00:00.000Z");
+    assertEquals(testObj.date.getTime(), new Date(1990, 10, 11).getTime());
   },
 });
 
@@ -22,19 +42,27 @@ test({
       @SerializeProperty({
         fromJsonStrategy: iso8601Date,
       })
-      date!: Date | string;
+      date!: Date;
     }
-    const testObj = new Test().fromJson(`{"date":"I am not a date!"}`);
-    assertEquals(typeof testObj.date, "string");
-    assertEquals(testObj.date, "I am not a date!");
+    try {
+      new Test().fromJson(`{"date":"I am not a date!"}`);
+      fail("Non date string did not error");
+    } catch (error) {
+      assertEquals(error.message, "Invalid date");
+    }
   },
 });
 
 test({
   name: "iso8601Date parses with milliseconds - 2020-12-31T12:00:00.300Z",
   fn() {
-    const testJson = `{"date":"2020-12-31T12:00:00.300Z"}`;
-    const testObj = JSON.parse(testJson, (_, v) => iso8601Date(v));
+    class Test extends Serializable {
+      @SerializeProperty({
+        fromJsonStrategy: iso8601Date,
+      })
+      public date!: Date;
+    }
+    const testObj = new Test().fromJson({ "date": "2020-12-31T12:00:00.300Z" });
     assert(testObj.date instanceof Date);
     assertEquals(testObj.date.toISOString(), "2020-12-31T12:00:00.300Z");
   },
@@ -43,38 +71,30 @@ test({
 test({
   name: "iso8601Date parses without milliseconds - 2020-12-31T12:00:00Z",
   fn() {
-    const testJson = `{"date":"2020-12-31T12:00:00"}`;
-    const testObj = JSON.parse(testJson, (_, v) => iso8601Date(v));
+    class Test extends Serializable {
+      @SerializeProperty({
+        fromJsonStrategy: iso8601Date,
+      })
+      public date!: Date;
+    }
+    const testObj = new Test().fromJson({ "date": "2020-12-31T12:00:00Z" });
     assert(testObj.date instanceof Date);
     assertEquals(testObj.date.toISOString(), "2020-12-31T12:00:00.000Z");
   },
 });
 
 test({
-  name: "iso8601Date parses with added timezone - 2020-12-31T12:00:00Z-6",
+  name: "iso8601Date parses with added timezone - 2020-12-31T00:00:00-05:00",
   fn() {
-    const testJson = `{"date":"2020-12-31T06:00:00Z-6"}`;
-    const testObj = JSON.parse(testJson, (_, v) => iso8601Date(v));
-    assert(testObj.date instanceof Date);
-    assertEquals(testObj.date.toISOString(), "2020-12-31T12:00:00.000Z");
-  },
-});
-
-test({
-  name: "iso8601Date parses time without `T` - 2020-06-29 12:00:00Z",
-  fn() {
-    const testJson = `{"date":"2020-12-31 12:00:00Z"}`;
-    const testObj = JSON.parse(testJson, (_, v) => iso8601Date(v));
-    assert(testObj.date instanceof Date);
-    assertEquals(testObj.date.toISOString(), "2020-12-31T12:00:00.000Z");
-  },
-});
-
-test({
-  name: "iso8601Date parses `2020-366` - leap year",
-  fn() {
-    const testJson = `{"date":"2020-366"}`;
-    const testObj = JSON.parse(testJson, (_, v) => iso8601Date(v));
+    class Test extends Serializable {
+      @SerializeProperty({
+        fromJsonStrategy: iso8601Date,
+      })
+      public date!: Date;
+    }
+    const testObj = new Test().fromJson(
+      { "date": "2020-12-31T00:00:00-05:00" },
+    );
     assert(testObj.date instanceof Date);
     assertEquals(testObj.date.toISOString(), "2020-12-31T00:00:00.000Z");
   },
