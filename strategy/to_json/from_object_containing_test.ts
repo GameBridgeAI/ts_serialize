9dc0e876bc857ee5082e8878dd40eec14f88017a
Scott Hardy <scott.hardy@mindbridge.ai>
2020-12-11 01:35:40 -0500

    another test;

--- strategy/to_json/from_object_containing_test.ts
+++ strategy/to_json/from_object_containing_test.ts
@@ -1,9 +1,10 @@
 // Copyright 2018-2020 Gamebridge.ai authors. All rights reserved. MIT license.
 
-import { assert, assertEquals, test } from "../../test_deps.ts";
+import { assertEquals, test } from "../../test_deps.ts";
 import { fromObjectContaining } from "./from_object_containing.ts";
 import { Serializable } from "../../serializable.ts";
 import { SerializeProperty } from "../../serialize_property.ts";
+import { toSerializable } from "../from_json/to_serializable.ts";
 
 test({
   name: "fromObjectContaining builds from Serializable",
@@ -63,6 +64,35 @@ test({
     assertEquals(
       new Test().toJSON(),
       `{"test":{"testing":[{"someClassProp":"test"},{"someClassProp":"test"},{"someClassProp":"test"}]}}`,
+    );
+  },
+});
+
+test({
+  only: true,
+  name: "fromObjectContaining works with nested strategies",
+  fn() {
+    class TheClass extends Serializable {
+      @SerializeProperty("the_class_prop")
+      theClassProp = "test";
+    }
+    class SomeClass extends Serializable {
+      @SerializeProperty()
+      someClassProp = "test";
+
+      @SerializeProperty({ fromJSONStrategy: toSerializable(TheClass) })
+      someOtherClassProp = new TheClass();
+    }
+
+    class Test extends Serializable {
+      @SerializeProperty({ toJSONStrategy: fromObjectContaining })
+      test: { [k: string]: SomeClass[] } = {
+        testing: [new SomeClass(), new SomeClass(), new SomeClass()],
+      };
+    }
+    assertEquals(
+      new Test().toJSON(),
+      `{"test":{"testing":[{"someClassProp":"test","someOtherClassProp":{"the_class_prop":"test"}},{"someClassProp":"test","someOtherClassProp":{"the_class_prop":"test"}},{"someClassProp":"test","someOtherClassProp":{"the_class_prop":"test"}}]}}`,
     );
   },
 });
