31e383bd7ebf62b2343988149bb1937c37d2f816
Scott Hardy <scott.hardy@mindbridge.ai>
2021-01-08 10:37:37 -0500

    bump deno and sd fixnig #96

--- examples/deno/deps.ts
+++ examples/deno/deps.ts
@@ -10,4 +10,4 @@ export {
   toSerializable,
   TransformKey,
 } from "https://deno.land/x/ts_serialize/mod.ts";
-export { readJson } from "https://deno.land/std@0.79.0/fs/mod.ts";
+export { readJson } from "https://deno.land/std@0.83.0/fs/mod.ts";
--- test_deps.ts
+++ test_deps.ts
@@ -7,4 +7,4 @@ export {
   assertEquals,
   assertStrictEquals,
   fail,
-} from "https://deno.land/std@0.80.0/testing/asserts.ts";
+} from "https://deno.land/std@0.83.0/testing/asserts.ts";
