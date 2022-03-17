18861ccf5ee6675028ab1409d758901b46f9957b
Scott Hardy <hardy613+github@gmail.com>
2022-03-17 23:57:53 +0000

    chore: remove commended out stuff

--- strategy/compose_strategy.ts
+++ strategy/compose_strategy.ts
@@ -1,13 +1,8 @@
 // Copyright 2018-2022 Gamebridge.ai authors. All rights reserved. MIT license.
-// import { JSONValue } from "../serializable.ts";
 
 /** Functions used when hydrating data */
 // deno-lint-ignore no-explicit-any
 export type Strategy = (value: any) => any;
-
-/** Functions used when dehydrating data */
-/// deno-lint-ignore no-explicit-any
-// export type ToJSONStrategy = (value: any) => JSONValue;
 
 /** Function to build a `fromJSONStrategy` or `toJSONStrategy`.
  * Converts value from functions provided as parameters
@@ -16,7 +11,6 @@ export function composeStrategy(
   ...fns: Strategy[]
 ): Strategy {
   return (
-    /// deno-lint-ignore no-explicit-any
     val: unknown,
   ): unknown => fns.reduce((acc, fn) => fn(acc), val);
 }
